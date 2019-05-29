9a7f5fc0becf882779f674a22285e4c597d7648d
Gauravjot Garaya <gauravjotg@gmail.com>
2019-05-29 03:09:52 -0700

    working app

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import Wallpapers from './wallpapers';
 
 class App extends React.Component {
-
+    
   state = {
-    url: 'https://www.reddit.com/r/wallpapers+wallpaper+widescreenwallpaper+wqhd_wallpaper/hot.json',
+    url: 'https://www.reddit.com/r/',
+    subreddit: 'wallpapers+wallpaper+widescreenwallpaper+wqhd_wallpaper',
+    sort: '/hot',
+    allSubreddit: 'wallpapers+wallpaper+widescreenwallpaper+wqhd_wallpaper',
+    subreddits: ['wallpaper','wallpapers','widescreenwallpaper','wqhd_wallpaper'],
     files: [],
     after: null,
-    before: null
-  }
+    before: null,
+    page: 1
+  };
 
   componentDidMount() {
-    fetch(this.state.url)
+    fetch(this.state.url + this.state.subreddit + this.state.sort + '.json')
     .then(res => res.json())
     .then((data) => {
       this.setState({files: data.data.children});
@@ -22,18 +27,34 @@ class App extends React.Component {
   }
 
   nextPage = ()=> {
-      fetch(this.state.url + "?count=25&after=" + this.state.after)
+      fetch(this.state.url + this.state.subreddit + this.state.sort + ".json?count="+ (this.state.page * 25) +"&after=" + this.state.after)
       .then(res => res.json())
       .then((data) => {
         this.setState({files: data.data.children});
         this.setState({after: data.data.after});
         this.setState({before: data.data.before});
+        this.setState({page: this.state.page + 1});
       })
       .catch(console.log)
   }
 
   prevPage = ()=> {
-    fetch(this.state.url + "?count=25&before=" + this.state.before)
+    fetch(this.state.url + this.state.subreddit + this.state.sort + ".json?count="+ (((this.state.page - 1) * 25) - 1) +"&before=" + this.state.before)
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({files: data.data.children});
+      this.setState({after: data.data.after});
+      this.setState({before: data.data.before});
+      if (this.state.page > 1) {
+        this.setState({page: this.state.page - 1});
+      }
+    })
+    .catch(console.log)
+  }
+
+  changeSubreddit(sub) {
+    this.setState({subreddit: sub});
+    fetch(this.state.url + sub + this.state.sort + '.json')
     .then(res => res.json())
     .then((data) => {
       this.setState({files: data.data.children});
@@ -44,26 +65,43 @@ class App extends React.Component {
   }
 
   render () {
-    let pagingJSX;
-    const buttonNext = <input className="btn btn-primary" type="submit" value="Next" onClick={this.nextPage}/>;
-    const buttonPrev = <input className="btn btn-primary" type="submit" value="Prev" onClick={this.prevPage}/>;
-        console.log(this.state.after);
-        console.log(this.state.before);
-    if (this.state.after === null) {
-        // last page
-        pagingJSX = <div>{buttonPrev}</div>;
-    } else if (this.state.before === null) {
-        // first page
-        pagingJSX = <div>{buttonNext}</div>;
+    let contentJSX;
+    if (this.state.files.length > 0) {
+      let pagingJSX;
+      const buttonNext = <button className="btn btn-secondary" type="submit" onClick={this.nextPage}>Next</button>;
+      const buttonPrev = <button className="btn btn-secondary" type="submit" onClick={this.prevPage}>Previous</button>;
+      if (this.state.after === null) {
+          // last page
+          pagingJSX = <div>{buttonPrev}</div>;
+      } else if (this.state.before === null) {
+          // first page
+          pagingJSX = <div>{buttonNext}</div>;
+      } else {
+          // in between pages
+          pagingJSX = <div>{buttonPrev} <span className="p-3 text-black-50">Page {this.state.page}</span> {buttonNext}</div>;
+      }
+      contentJSX = <div className="m-2"><Wallpapers files={this.state.files}/><br/><div className="center-block m-2">{pagingJSX}</div></div>;
     } else {
-        // in between pages
-        pagingJSX = <div>{buttonPrev} {buttonNext}</div>;
+      contentJSX = 'Loading...'
     }
 
     return (
       <div className="container">
-        <Wallpapers files={this.state.files}/>
-        <div className="center">{pagingJSX}</div>
+        <br/>
+        <div className="dropdown m-2">
+          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            r/{this.state.subreddit}
+          </button>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a className="dropdown-item" href="#subChange" onClick={() => this.changeSubreddit(this.state.allSubreddit)}>All Below</a>
+            {this.state.subreddits.map((subreddit) => (
+              <a className="dropdown-item" href="#subChange" onClick={() => this.changeSubreddit(subreddit)}>r/{subreddit}</a>
+            ))}
+          </div>
+        </div>
+        <br/>
+        {contentJSX}
+        <br/>
       </div>
     );
   }
--- src/components/wallpaper_item.js
+++ src/components/wallpaper_item.js
@@ -2,23 +2,26 @@ import React from 'react';
 
 const WallpaperItem = ({file}) => {
     if (file.data.preview) {
-        let previewImage = file.data.preview ? file.data.preview.images[0].resolutions[2].url : "http://dummyimage.com";
-        previewImage = previewImage.replace(/&amp;/g,"&");
-        return (
-            <div className="col-sm-12 col-md-4 col-lg-3">
-                <div className="card">
-                    <div className="card-img-top">
-                        <img src={previewImage} width="100%" alt={file.data.title}/>
+        if( file.data.preview.enabled) {
+            console.log(file);
+            let previewImage = file.data.preview ? file.data.preview.images[0].resolutions[2].url : "http://dummyimage.com";
+            previewImage = previewImage.replace(/&amp;/g,"&");
+            return (
+                <div className="col-sm-12 col-md-4 col-lg-3">
+                    <div className="card">
+                        <div className="card-img-top">
+                            <img src={previewImage} width="100%" alt={file.data.title}/>
+                        </div>
                     </div>
+                    {file.data.name}
                 </div>
-                {file.data.name}
-            </div>
-        );
-    } else {
-        return (
-            <div></div>
-        );
+            );
+        }
     }
+    return (
+        <div></div>
+    );
+    
 } 
 
 export default WallpaperItem;
