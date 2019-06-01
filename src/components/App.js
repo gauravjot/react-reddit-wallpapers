6cc8460f1648647b281a4f5d14f830dca7701c60
Gauravjot Garaya <gauravjotg@gmail.com>
2019-05-31 21:04:36 -0700

    version 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
+import _ from 'lodash';
 import React from 'react';
 import Wallpapers from './wallpapers';
+import SearchBar from './search_bar';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.wallpaperSubreddits = 'wallpapers+wallpaper+widescreenwallpaper+wqhd_wallpaper';
+    this.memesSubreddits = 'memes+dankmemes+memeeconomy+animemes';
+    this.allSubreddits = 'wallpapers+wallpaper+widescreenwallpaper+wqhd_wallpaper+memes+dankmemes+memeeconomy+animemes';
+    this.subredditsArray = ['wallpaper','wallpapers','widescreenwallpaper','wqhd_wallpaper','memes', 'dankmemes', 'memeeconomy','animemes'];
+    this.url = 'https://www.reddit.com/r/';
+    this.sorts = ['hot','new','top','controversial','rising'];
+  }
     
   state = {
-    url: 'https://www.reddit.com/r/',
-    subreddit: 'wallpapers+wallpaper+widescreenwallpaper+wqhd_wallpaper',
-    sort: '/hot',
-    allSubreddit: 'wallpapers+wallpaper+widescreenwallpaper+wqhd_wallpaper',
-    subreddits: ['wallpaper','wallpapers','widescreenwallpaper','wqhd_wallpaper'],
+    currentSubreddit: 'wallpapers+wallpaper+widescreenwallpaper+wqhd_wallpaper',
+    sort: 'hot',
     files: [],
     after: null,
     before: null,
@@ -16,35 +25,30 @@ class App extends React.Component {
   };
 
   componentDidMount() {
-    fetch(this.state.url + this.state.subreddit + this.state.sort + '.json')
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({files: data.data.children});
-      this.setState({after: data.data.after});
-      this.setState({before: data.data.before});
-    })
-    .catch(console.log)
+    this.changeSubreddit(this.state.currentSubreddit);
   }
 
   nextPage = ()=> {
-      fetch(this.state.url + this.state.subreddit + this.state.sort + ".json?count="+ (this.state.page * 25) +"&after=" + this.state.after)
+      fetch(this.url + this.state.currentSubreddit + "/" + this.state.sort + ".json?count="+ (this.state.page * 25) +"&after=" + this.state.after)
       .then(res => res.json())
       .then((data) => {
         this.setState({files: data.data.children});
         this.setState({after: data.data.after});
         this.setState({before: data.data.before});
         this.setState({page: this.state.page + 1});
+      window.scrollTo(0,0);
       })
       .catch(console.log)
   }
 
   prevPage = ()=> {
-    fetch(this.state.url + this.state.subreddit + this.state.sort + ".json?count="+ (((this.state.page - 1) * 25) - 1) +"&before=" + this.state.before)
+    fetch(this.url + this.state.currentSubreddit + "/" + this.state.sort + ".json?count="+ (((this.state.page - 1) * 25) - 1) +"&before=" + this.state.before)
     .then(res => res.json())
     .then((data) => {
       this.setState({files: data.data.children});
       this.setState({after: data.data.after});
       this.setState({before: data.data.before});
+    window.scrollTo(0,0);
       if (this.state.page > 1) {
         this.setState({page: this.state.page - 1});
       }
@@ -53,23 +57,49 @@ class App extends React.Component {
   }
 
   changeSubreddit(sub) {
-    this.setState({subreddit: sub});
+    this.setState({files: []});
+    this.setState({currentSubreddit: sub});
     this.setState({page: 1})
-    fetch(this.state.url + sub + this.state.sort + '.json')
+    fetch(this.url + sub + "/" + this.state.sort + '.json')
     .then(res => res.json())
     .then((data) => {
       this.setState({files: data.data.children});
       this.setState({after: data.data.after});
       this.setState({before: data.data.before});
+    window.scrollTo(0,0);
     })
     .catch(console.log)
   }
 
+  changeSort(sort) {
+    this.setState({files: []});
+    this.setState({sort: sort});
+    this.setState({page: 1})
+    fetch(this.url + this.state.currentSubreddit + "/" + sort + '.json')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({files: data.data.children});
+      this.setState({after: data.data.after});
+      this.setState({before: data.data.before});
+    window.scrollTo(0,0);
+    })
+    .catch(console.log)
+  }
+
+  searchSubreddit(subreddit) {
+    if (subreddit.length !== 0) {
+      this.changeSubreddit(subreddit);
+    } else {
+      this.changeSubreddit(this.allSubreddits);
+    }
+  }
+
   render () {
+    const searchSubreddit = _.debounce((term) => {this.searchSubreddit(term)}, 600);
     let contentJSX;
     if (this.state.files.length > 0) {
       let pagingJSX;
-      const buttonNext = <button className="btn btn-secondary" type="submit" onClick={this.nextPage}>Next</button>;
+      const buttonNext = <button className="btn btn-primary" type="submit" onClick={this.nextPage}>Next</button>;
       const buttonPrev = <button className="btn btn-secondary" type="submit" onClick={this.prevPage}>Previous</button>;
       if (this.state.after === null) {
           // last page
@@ -83,26 +113,51 @@ class App extends React.Component {
       }
       contentJSX = <div className="m-2"><Wallpapers files={this.state.files}/><br/><div className="center-block m-2">{pagingJSX}</div></div>;
     } else {
-      contentJSX = 'Loading...'
+      contentJSX = <div className="p-2"><center>Loading...</center></div>;
+    }
+
+    let currentSubreddit;
+    if (this.state.currentSubreddit === this.wallpaperSubreddits) {
+      currentSubreddit = "All Wallpapers";
+    } else if (this.state.currentSubreddit === this.memesSubreddits) {
+      currentSubreddit = "All Memes";
+    } else {
+      currentSubreddit = "r/" + this.state.currentSubreddit;
     }
 
     return (
       <div className="container">
         <br/>
-        <div className="dropdown m-2">
-          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            r/{this.state.subreddit}
-          </button>
-          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a className="dropdown-item" href="#subChange" onClick={() => this.changeSubreddit(this.state.allSubreddit)}>All Below</a>
-            {this.state.subreddits.map((subreddit, index) => (
-              <a className="dropdown-item" key={index} href="#subChange" onClick={() => this.changeSubreddit(subreddit)}>r/{subreddit}</a>
-            ))}
+        <div>
+          <div className="dropdown m-2" style={{display: "inline"}}>
+            <button className="btn btn-outline-success dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              {currentSubreddit} &nbsp;
+            </button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a className="dropdown-item" href="#subChange" onClick={() => this.changeSubreddit(this.wallpaperSubreddits)}>All Wallpapers</a>
+              <a className="dropdown-item" href="#subChange" onClick={() => this.changeSubreddit(this.memesSubreddits)}>All Memes</a>
+              {this.subredditsArray.map((subreddit, index) => (
+                <a className="dropdown-item" key={index} href="#subChange" onClick={() => this.changeSubreddit(subreddit)}>r/{subreddit}</a>
+              ))}
+            </div>
           </div>
+          <div className="dropdown m-2" style={{display: "inline"}}>
+            <button className="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              {this.state.sort} &nbsp;
+            </button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              {this.sorts.map((sort, index) => (
+                <a className="dropdown-item" key={index} href="#subChange" onClick={() => this.changeSort(sort)}>r/{sort}</a>
+              ))}
+            </div>
+          </div>
+          <div className="m-3"></div>
+          <SearchBar onSearchTermChange={term => searchSubreddit(term)}/>
         </div>
         <br/>
         {contentJSX}
         <br/>
+        <footer><p><center>Open-source available on <a href="https://github.com/gauravjot/react-reddit-wallpapers">Github</a>.</center></p></footer>
       </div>
     );
   }
--- src/components/wallpaper_item.js
+++ src/components/wallpaper_item.js
@@ -3,17 +3,31 @@ import React from 'react';
 const WallpaperItem = ({file}) => {
     if (file.data.preview) {
         if( file.data.preview.enabled) {
-            console.log(file);
             let previewImage = file.data.preview ? file.data.preview.images[0].resolutions[2].url : "http://dummyimage.com";
             previewImage = previewImage.replace(/&amp;/g,"&");
+            let subreddit = "https://reddit.com/r/" + file.data.subreddit;
+            let post_link = "https://reddit.com" + file.data.permalink;
             return (
                 <div className="col-sm-12 col-md-6 col-lg-4">
+                    <div className=" card-link">
                     <div className="card">
                         <div className="card-img-top">
-                            {(file.data.over_18) ? <div>NSFW</div> : <img src={previewImage} width="100%" alt={file.data.title}/>}
+                            <a href={post_link} target="_blank" rel="noopener noreferrer"><button className="btn btn-secondary btn-sm top-right-float">View Post</button></a>
+                            <a href={file.data.url} target="_blank" rel="noopener noreferrer">
+                                {(file.data.over_18) ? <div>NSFW</div> : <img src={previewImage} className="card-img-top" width="100%" alt={file.data.title}/>}
+                            </a>
+                        </div>
+                        <div className="card-body">
+                            <div className="card-title">   
+                                <a href={file.data.url} target="_blank" rel="noopener noreferrer">{file.data.title}</a>
+                            </div>
+                            <p className="card-text">
+                                Resolution: {file.data.preview.images[0].source.width}x{file.data.preview.images[0].source.height}
+                                <br/>Posted on: <a href={subreddit} target="_blank" rel="noopener noreferrer">r/{file.data.subreddit}</a>
+                            </p>
                         </div>
                     </div>
-                    {file.data.name}
+                    </div>
                 </div>
             );
         }
