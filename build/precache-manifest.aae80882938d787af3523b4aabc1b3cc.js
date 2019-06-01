a8ca3d84bf78b6774f7a2e248579606a2cdc577b
Gauravjot Garaya <gauravjotg@gmail.com>
2019-05-31 21:06:35 -0700

    version 1

---
+++ build/precache-manifest.aae80882938d787af3523b4aabc1b3cc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e9b42abe376fa8b2cc55a53122d496",
+    "url": "/index.html"
+  },
+  {
+    "revision": "157f9282cb929b6ffec3",
+    "url": "/static/js/2.5b5d6d0c.chunk.js"
+  },
+  {
+    "revision": "51696827d486b1df35fd",
+    "url": "/static/js/main.cda2e4d7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
